857feaff5201e0cb4298394631b3d9ef1b7d4ba2
Andrew Dirksen <andrew@dirksen.com>
2020-09-30 09:25:22 -0700

    script to start server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.static(`${__dirname}/dist/`));
+app.listen(port);
