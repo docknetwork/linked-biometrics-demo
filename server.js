2f1477b8b19d424075b95971c847364ce63edd37
Andrew Dirksen <andrew@dirksen.com>
2020-09-30 10:12:39 -0700

    deployed server should listen only on localhost by default

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'localhost';
 const app = express();
 app.use(express.static(`${__dirname}/dist/`));
-app.listen(port);
+app.listen(port, host);
